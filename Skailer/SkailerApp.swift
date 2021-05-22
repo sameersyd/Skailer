61cc8e161db840d2a0279825569acfb16cbe6404
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-05-22 13:49:12 +0530

    Initial Commit

---
+++ Skailer/SkailerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkailerApp.swift
+//  Skailer
+//
+//  Created by Sameer Nawaz on 22/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SkailerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
